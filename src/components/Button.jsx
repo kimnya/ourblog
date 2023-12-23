027f23075801385689861ac8312df0351e75d9d9
김냐 <ymca582@icloud.com>
2023-12-23 19:57:57 +0900

    design: 버튼 컴포넌트 생성 #1

---
+++ src/components/Button.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { palette } from "../styles/palette";
+
+const ButtonStyled = styled.button`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border: none;
+  outline: none;
+
+  ${({
+    width = "50px",
+    height = "25px",
+    $buttonColor = "mainGreen",
+    $fontColor = "white",
+    $fontSize = "12px",
+    fontWeight = "bold",
+    $borderRadius = "4px",
+  }) => css`
+    width: ${width};
+    height: ${height};
+    color: ${palette[$fontColor]};
+    font-weight: ${fontWeight};
+    font-size: ${$fontSize};
+    background-color: ${palette[$buttonColor]};
+    border-radius: ${$borderRadius};
+  `}
+`;
+
+const Button = ({ children, ...rest }) => {
+  return (
+    <>
+      <ButtonStyled {...rest}>{children}</ButtonStyled>
+    </>
+  );
+};
+
+export default Button;
