7fb350a918fce40465606e1c0fb2f5509290d256
김냐 <ymca582@icloud.com>
2023-12-26 23:56:55 +0900

    design:Pure CSS로 darken,lighten 기능구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
-import Button from './components/Button';
-import Input from './components/Input';
 import { GlobalStyle } from './styles/GlobalStyle';
+import Register from './page/Register';
+
+const Wrap = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  border: 1px solid #000;
+`;
 
 const FlexBox = styled.div`
   display: flex;
@@ -17,28 +25,11 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <div style={{ display: 'flex' }}>
+      <Wrap>
         <FlexBox>
-          <Button width='155px' height='34px' $borderRadius='none'>
-            회원가입
-          </Button>
-          <Button $fontColor='mainGray'>로그인</Button>
-          <Button>수정</Button>
-          <Button width='70px' $buttonColor='mainOrange'>
-            회원탈퇴
-          </Button>
+          <Register />
         </FlexBox>
-        <FlexBox>
-          <Input $placeholder='userName' />
-          <Input $placeholder='eamil' />
-          <Input type='password' $placeholder='password' />
-          <Input type='password' $placeholder='password확인' />
-          <Input $placeholder='nickName' />
-          <Button width='362px' height='29px' $borderRadius='none'>
-            회원가입
-          </Button>
-        </FlexBox>
-      </div>
+      </Wrap>
     </>
   );
 }
