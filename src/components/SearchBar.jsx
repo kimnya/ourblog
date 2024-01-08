47d7625440c9e4f0fd5f2fecd7f5fe0b90cf72e9
김냐 <ymca582@icloud.com>
2024-01-08 13:43:45 +0900

    design: 메인페이지 게시물리스트 구현 #22

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import Input from './Input';
+import styled from 'styled-components';
+
+const Form = styled.form`
+  > label {
+    display: none;
+  }
+`;
 
 const SearchBar = () => {
-  return <Input width='450px' height='50px' $placeHoder='검색할 단어를 입력해주세요.' autoFocus />;
+  const [searchVelue, setSearchVelue] = useState();
+  return (
+    <>
+      <Form>
+        <label htmlFor='searchVelue'>검색창</label>
+        <Input width='450px' height='50px' $placeHoder='검색할 단어를 입력해주세요.' autoFocus />;
+      </Form>
+    </>
+  );
 };
 
 export default SearchBar;
