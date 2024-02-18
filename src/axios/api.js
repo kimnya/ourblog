e5f283390d9b26bb0908f79664ce7c8db969b177
김냐 <ymca582@icloud.com>
2024-02-18 21:20:06 +0900

    chore: api주소 변수로 저장

--- src/axios/api.js
+++ src/axios/api.js
@@ -3,7 +3,7 @@ import { getCookie, setCookie } from '../components/cookie';
 
 // 게시물리스트 호출
 export const articleListRead = async () => {
-  const response = await axios.get('https://port-0-ourblog-am952nlsn8cg1z.sel5.cloudtype.app/posting/list', {
+  const response = await axios.get('/api/posting/list', {
     params: { searchText: '' },
   });
   return response;
