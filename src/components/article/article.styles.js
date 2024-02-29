7619378e8e3a77949f86bb8c28aa23f4d917048d
김냐 <ymca582@icloud.com>
2024-03-01 00:29:50 +0900

    design: BlogNameTag 마진 수정

--- src/components/article/article.styles.js
+++ src/components/article/article.styles.js
@@ -57,7 +57,7 @@ export const ArticleListBoxStyle = styled.div`
 `;
 
 export const BlogNameTag = styled.p`
-  margin: 30px auto;
+  margin: 30px 30px;
   color: ${palette.mainGreen};
   font-weight: bold;
   font-size: 22px;
